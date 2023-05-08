8ead52443c9d56458d77e8ca91bb2f81a8024e1f
chk904904 <60486519+chk904904@users.noreply.github.com>
2023-05-08 13:47:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogs8Y6n3-1X7xF7dL7fXaYgvuoNs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1rl8h3uekQ1fZfYO4luWD-HwHCAwvc-Y02W-_QcJhDM',
+      useTemplateId: 'xt7Z3ziudBc0k7bKm5qPOnuv8QteQXkE4PpC9cW6yPU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-23',
       festivals: [
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-05-08' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
